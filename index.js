06f170f168c687771a8e5ad4d0883a6790c3d5bb
James Marotta <james.marotta@outlook.com>
2020-09-13 22:00:03 +1000

    Realy urls wont work

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const axios = require('axios')
 const cors = require('cors')
 app.use(cors())
 
-async function main(mode) {
+async function main({ engine, mode, searchQuery }) {
   const pages = ['Page01', 'Page02', 'Page03', 'Page04', 'Page05']
 
-  const engine = {
+  const engineTypes = {
     google: 'Google',
     bing: 'Bing'
   }
@@ -18,17 +18,22 @@ async function main(mode) {
     bing: ['.b_algo h2 a', '#b_results']
   }
 
-  const results = ['jipped']
+  // const urls = {
+  //   google: `https://google.com/search?q=${searchQuery}`,
+  //   bing: `https://bing.com/search?q=${searchQuery}`
+  // }
 
-  const currentEngine = engine[mode]
+  const currentEngine = engineTypes[engine]
+
+  // Can't convert the nodelist to an Array correctly for real urls
+  // const fancyUrl = urls[engine]
 
   const data = Promise.all(
     pages.map(async page => {
       const url = `https://infotrack-tests.infotrack.com.au/${currentEngine}/${page}.html`
-      const currentSelector = selectors[mode]
 
+      const currentSelector = selectors[engine]
       try {
-        // can i still awai there
         const res = await axios.get(url)
         const $ = cheerio.load(res.data)
         const selectors = $(currentSelector[0], currentSelector[1]).get()
@@ -38,7 +43,8 @@ async function main(mode) {
         const targets = selectors.flatMap((x, i) =>
           $(x).attr('href').startsWith(href) ? [i] : []
         )
-        return { [page]: targets }
+
+        return { [page.slice(-2)]: String(targets) }
       } catch (e) {
         console.error('FAILED', { status: e.status, msg: e.message })
       }
@@ -50,12 +56,23 @@ async function main(mode) {
 // https://expressjs.com/en/starter/basic-routing.html
 app.get('/:engine/:mode/:searchQuery', async (request, response) => {
   const { engine, mode, searchQuery } = request.params
-
-  const x = await main(engine)
-  response.send({ x })
+  console.log({ engine, mode, searchQuery })
+  const x = await main({ engine, mode, searchQuery })
+  response.send(x)
 })
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+// testing
+// const fn = async () => {
+//   const engine = 'google'
+//   const mode = 'fancy'
+//   const searchQuery = 'online title search'
+
+//   console.log(await main({ engine, mode, searchQuery }))
+// }
+
+// fn()
